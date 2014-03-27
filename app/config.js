2bde21946bcb536f58105f45922cd113c2a40877
Hack Reactor Students <students@hackreactor.com>
2014-03-27 15:46:26 -0700

    Removed build folder from commit

--- app/config.js
+++ app/config.js
@@ -4,8 +4,7 @@ var db = mongoose.connection;
 
 db.on('error', console.error);
 
-// var database = process.env.PORT ? "mongodb://MongoLab-s:sbJm8VBzirDe47F8_juF86JjpQtXOFmgqhdEJaj9nDI-@ds030607.mongolab.com:30607/MongoLab-s" : 'mongodb://localhost/test';
-var database = "mongodb://MongoLab-s:sbJm8VBzirDe47F8_juF86JjpQtXOFmgqhdEJaj9nDI-@ds030607.mongolab.com:30607/MongoLab-s";
+var database = process.env.MONGO_URL || 'mongodb://localhost/test';
 
 mongoose.connect(database);
 
